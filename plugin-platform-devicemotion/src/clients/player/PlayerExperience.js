6265edf1d9a9fc2dc8c7fdb97a6cd742874e7728
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2020-08-31 15:57:00 +0200

    updated logger example

--- plugin-platform-devicemotion/src/clients/player/PlayerExperience.js
+++ plugin-platform-devicemotion/src/clients/player/PlayerExperience.js
@@ -48,7 +48,7 @@ class PlayerExperience extends AbstractExperience {
 
     this.rafId = window.requestAnimationFrame(() => {
       render(html`
-        <h1 style="padding: 20px; margin: 0">${this.client.type} [id: ${this.client.id}]</h1>
+        <h1 style="padding: 20px; margin: 0">@soundworks/plugin-platform [id: ${this.client.id}]</h1>
         <sc-signal style="margin: 20px;" id="plot"></sc-signal>
       `, this.$container);
     });
